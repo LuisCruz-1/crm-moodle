4e3aa130af75314ea72f08491b6906c6718abcf0
LuisCruz-1 <luiscruzm.ec@gmail.com>
2026-05-14 12:39:43 -0500

    arreglando subnav

--- resources/js/Components/CommsSubnav.jsx
+++ resources/js/Components/CommsSubnav.jsx
@@ -4,7 +4,7 @@ import Card from '@/Components/Card';
 export default function CommsSubnav() {
     return (
         <Card className="!p-0 overflow-hidden mb-6">
-            <div className="flex flex-wrap gap-6 px-6 pt-2 bg-gray-50/50 border-b border-gray-200">
+            <div className="flex flex-wrap gap-6 p-4 bg-gray-50/50 border-gray-200">
                 <NavLink href={route('comms.templates.index')} active={route().current('comms.templates.*')} className="pb-3 pt-2">
                     Plantillas de Correo
                 </NavLink>
--- resources/js/Components/CrmSubnav.jsx
+++ resources/js/Components/CrmSubnav.jsx
@@ -4,7 +4,7 @@ import Card from '@/Components/Card';
 export default function CrmSubnav() {
     return (
         <Card className="!p-0 overflow-hidden mb-6">
-            <div className="flex flex-wrap gap-6 px-6 pt-2 bg-gray-50/50 border-b border-gray-200">
+            <div className="flex flex-wrap gap-6 p-4 bg-gray-50/50 border-gray-200">
                 <NavLink href={route('crm.kanban.index')} active={route().current('crm.kanban.*')} className="pb-3 pt-2">
                     Kanban
                 </NavLink>
